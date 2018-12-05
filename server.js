5572742343b9dd37cc9e95817ed028ce76bb0bd7
yexuan <yexuan@yexuan.site>
2018-12-05 22:54:29 +0800

    add function send mail...

--- server.js
+++ server.js
@@ -52,3 +52,9 @@ server.use('/users', function (req, res) { //用户
     if (req.query.judge == 0) users.login(req, res);
     if (req.query.judge == null) res.redirect('./WWW/404/QYZQ.html');
 });
+
+server.use('/mail',function(req,res){    
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    if(req.query.judge==0)  mail.register(req,res); 
+    if(req.query.judge==null) res.redirect('./WWW/404/QYZQ.html');
+})
